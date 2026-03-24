3ee481191c643994e5b1c6f35e51cfe0c1bd5660
Pierella David <dpierella@cyberpeaceinstitute.org>
2026-03-24 17:00:36 +0100

    Initialization of the project: - Map loading - Search by name

---
+++ src/lib/db.js
@@ -0,0 +1,16 @@
+import pg from 'pg'
+
+const { Pool } = pg
+
+globalThis.pgPool = globalThis.pgPool || new Pool({
+    host: import.meta.env.SCW_DB_HOST,
+    user: import.meta.env.SCW_DB_USER,
+    password: import.meta.env.SCW_DB_PASS,
+    database: import.meta.env.SCW_DB_NAME,
+    port: import.meta.env.SCW_DB_PORT,
+    ssl: { rejectUnauthorized: false },
+    max: 10,
+    idleTimeoutMillis: 30000
+})
+
+export const pool = globalThis.pgPool
