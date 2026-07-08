69f7a96913f1e58c9e08f52f1a18541897d94928
Siegfried EHRET <sehret@protect.ngo>
2026-07-08 22:19:15 +0200

    🎁 trying with connection object

--- src/lib/dbDrizzle.ts
+++ src/lib/dbDrizzle.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-const encodedPass = encodeURIComponent(process.env.SCW_DB_PASS || "");
-
-const dbUrl = `postgresql://${process.env.SCW_DB_USER}:${encodedPass}@${process.env.SCW_DB_HOST}:${process.env.SCW_DB_PORT}/${process.env.SCW_DB_NAME}?sslmode=no-verify`;
-
-export const db = drizzle(dbUrl);
+export const db = drizzle({
+  connection: {
+    user: process.env.SCW_DB_USER,
+    password: process.env.SCW_DB_PASS,
+    host: process.env.SCW_DB_HOST,
+    port: parseInt(process.env.SCW_DB_PORT ?? "5432", 10),
+    database: process.env.SCW_DB_NAME,
+    ssl: false,
+  },
+});
